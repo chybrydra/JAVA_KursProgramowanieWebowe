a1ce613079ceb10b60613040343aeb65d396e4f4
Bandyta <lukasz.grymulski@gmail.com>
2018-12-02 00:49:49 +0100

    odpalenie projektu na prowizoryczym serwerze

---
+++ Projekt2HelloServer/src/main/java/pl/lukaszgrymulski/helloServer/HelloServerApplication.java
@@ -0,0 +1,12 @@
+package pl.lukaszgrymulski.helloServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServerApplication.class, args);
+	}
+}
