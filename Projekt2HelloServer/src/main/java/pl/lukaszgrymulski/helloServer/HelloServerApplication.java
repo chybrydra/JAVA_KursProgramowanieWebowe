6ee7fbcc73595df546154a3d8d524430bdf101be
Bandyta <lukasz.grymulski@gmail.com>
2018-12-02 01:08:54 +0100

    odpalenie serwera

--- Projekt2HelloServer/src/main/java/pl/lukaszgrymulski/helloServer/HelloServerApplication.java
+++ Projekt2HelloServer/src/main/java/pl/lukaszgrymulski/helloServer/HelloServerApplication.java
@@ -1,8 +1,17 @@
 package pl.lukaszgrymulski.helloServer;
 
+import reactor.netty.http.server.HttpServer;
+
 public class HelloServerApplication {
 
 	public static void main(String[] args) {
+		//tworzenie serwera
+
+		//I. odpalenie zerwera
+		HttpServer server = HttpServer.create();
+
+		//II. powiedzenie "czego ma nasłuchiwać serwer"
+		
 
 	}
 }
