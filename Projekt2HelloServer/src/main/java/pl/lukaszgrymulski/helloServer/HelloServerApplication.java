b747506c7c4f6dd9a9fb9303f179ce737552b41f
Bandyta <lukasz.grymulski@gmail.com>
2018-12-02 01:03:26 +0100

    usuniecie niepotrzebnych linii kodu

--- Projekt2HelloServer/src/main/java/pl/lukaszgrymulski/helloServer/HelloServerApplication.java
+++ Projekt2HelloServer/src/main/java/pl/lukaszgrymulski/helloServer/HelloServerApplication.java
@@ -1,12 +1,8 @@
 package pl.lukaszgrymulski.helloServer;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class HelloServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloServerApplication.class, args);
+
 	}
 }
