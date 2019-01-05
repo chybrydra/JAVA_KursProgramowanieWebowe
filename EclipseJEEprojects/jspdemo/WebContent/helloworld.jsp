e3f4f1fc142f255cb1fcaf2d65be4570ee5aeb92
Bandyta <lukasz.grymulski@gmail.com>
2019-01-05 19:11:12 +0100

    create first JSP project

---
+++ EclipseJEEprojects/jspdemo/WebContent/helloworld.jsp
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+<title>Hello world - my first JPS app</title>
+</head>
+
+<body>
+<h3>Hello world, Java app here!</h3>
+The time on the server is: <%= new java.util.Date() %>
+</body>
+
+</html>
--- Projekt2HelloServer/src/main/java/pl/lukaszgrymulski/helloServer/HelloServerApplication.java
+++ Projekt2HelloServer/src/main/java/pl/lukaszgrymulski/helloServer/HelloServerApplication.java
@@ -1,17 +1,22 @@
 package pl.lukaszgrymulski.helloServer;
 
+import reactor.core.publisher.Flux;
 import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.HttpServerRoutes;
+
+import java.time.Duration;
 
 public class HelloServerApplication {
 
 	public static void main(String[] args) {
 		//tworzenie serwera
 
-		//I. odpalenie zerwera
-		HttpServer server = HttpServer.create();
 
-		//II. powiedzenie "czego ma nasłuchiwać serwer"
-		
-
-	}
+        HttpServer.create()
+                .host("localhost")
+                .port(8080)
+                .handle((req, res) -> res.sendObject(Flux.just("hello")))
+                .bind()
+                .block(Duration.ofMinutes(60));
+    }
 }
