0937964971db2478e22f5d557e8ac6afa6c5629a
Bandyta <lukasz.grymulski@gmail.com>
2019-01-09 00:32:14 +0100

    extend jsf form, add CSS

--- EclipseJEEprojects/JSF-managed-beans/src/pl/lukaszgrymulski/Student.java
+++ EclipseJEEprojects/JSF-managed-beans/src/pl/lukaszgrymulski/Student.java
@@ -1,14 +1,41 @@
 package pl.lukaszgrymulski;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class Student {
 	private String firstName;
 	private String lastName;
+	private String country;
+	private String age;
+	
+	//listOfAgeRanges for the second drop-down list
+	List<String> ageRangesList;
+	
 	
 	public Student() {
+		ageRangesList = new ArrayList();
+		ageRangesList.add("16 or less");
+		ageRangesList.add("17-20");
+		ageRangesList.add("21-24");
+		ageRangesList.add("25-29");
+		ageRangesList.add("30-39");
+		ageRangesList.add("40 or more");
 		
+	}	
+
+	public List<String> getAgeRangesList() {
+		return ageRangesList;
+	}	
+	
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
 	}
 
 	public String getFirstName() {
@@ -26,6 +53,15 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
 	
 	
 }
