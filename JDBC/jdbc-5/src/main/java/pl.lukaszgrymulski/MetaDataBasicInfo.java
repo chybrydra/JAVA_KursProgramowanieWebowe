a8775876ec1f8476f46f2c945c0aa684422ef871
Bandyta <lukasz.grymulski@gmail.com>
2019-01-14 00:30:55 +0100

    obtain db MetaData

---
+++ JDBC/jdbc-5/src/main/java/pl.lukaszgrymulski/MetaDataBasicInfo.java
@@ -0,0 +1,37 @@
+package pl.lukaszgrymulski;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MetaDataBasicInfo {
+
+    private static Connection connection;
+
+    public static void main(String[] args) {
+        try {
+            DBInfo db = new DBInfo();
+            connection = DriverManager.getConnection(db.getUrl(), db.getUser(), db.getPassword());
+
+            DatabaseMetaData dbmd = connection.getMetaData();
+
+            System.out.println("DB Product Name: " + dbmd.getDatabaseProductName());
+            System.out.println("DB Product Version: " + dbmd.getDatabaseProductVersion());
+            System.out.println("Driver Name: " + dbmd.getDriverName());
+            System.out.println("Driver Version: " + dbmd.getDriverVersion());
+            System.out.println("UserName: " + dbmd.getUserName());
+            System.out.println();
+            System.out.println("URL: " + dbmd.getURL());
+            System.out.println("SQL Keywords: " + dbmd.getSQLKeywords());
+            System.out.println("Numeric Functions: " + dbmd.getNumericFunctions());
+            System.out.println("Catalog Separator: " + dbmd.getCatalogSeparator());
+            System.out.println("Schema Term:" + dbmd.getSchemaTerm());
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
