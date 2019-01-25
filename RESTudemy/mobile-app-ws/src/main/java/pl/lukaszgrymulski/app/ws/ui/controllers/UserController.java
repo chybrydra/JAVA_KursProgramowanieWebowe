96369a46b195958df6fdd30c6f52f079416c061f
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-01-25 14:28:46 +0100

    create get method with different http status

--- RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
+++ RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
@@ -1,6 +1,8 @@
 package pl.lukaszgrymulski.app.ws.ui.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import pl.lukaszgrymulski.app.ws.ui.model.response.UserRest;
@@ -10,17 +12,14 @@ import pl.lukaszgrymulski.app.ws.ui.model.response.UserRest;
 public class UserController {
 	
 	@GetMapping //bind this method to HTTP request
-	public UserRest getUsers(
+	public String getUsers(
 			@RequestParam(value="page", defaultValue="1") int page, 
 			@RequestParam(value="limit", defaultValue="50") int limit,
 			@RequestParam(value="sort", defaultValue="desc", required=false) String sort
 			) {
-		UserRest returnValue = new UserRest();
-		returnValue.setEmail("test@test.com");
-		returnValue.setFirstName("Tommy");
-		returnValue.setLastName("Versetti");
-		returnValue.setUserId("1wD15gG2gE");
-		return returnValue;
+		return "get users was called with page = " + page 
+				+ ", limit = " + limit
+				+ ", sort = " + sort;
 	}
 	
 	@GetMapping(path="/{userId}", 
@@ -28,8 +27,13 @@ public class UserController {
 					MediaType.APPLICATION_XML_VALUE, 
 					MediaType.APPLICATION_JSON_VALUE
 				}) 
-	public String getUser(@PathVariable String userId) {
-		return "get user was called with userId = " + userId;
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@test.com");
+		returnValue.setFirstName("Tommy");
+		returnValue.setLastName("Versetti");
+		returnValue.setUserId("1wD15gG2gE");
+		return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@PostMapping 
