dfd57274823781e29f9d3d8ecda8564a7a05e041
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-01-25 16:53:42 +0100

    create PUT method body

--- RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
+++ RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
@@ -1,16 +1,25 @@
 package pl.lukaszgrymulski.app.ws.ui.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import pl.lukaszgrymulski.app.ws.ui.model.request.UpdateUserDetailsRequestModel;
 import pl.lukaszgrymulski.app.ws.ui.model.request.UserDetailsRequestModel;
 import pl.lukaszgrymulski.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users/
 public class UserController {
+	
+	Map <String, UserRest> users; //just to simulate DB without having one
 	
 	@GetMapping //bind this method to HTTP request
 	public String getUsers(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value="limit", defaultValue="50") int limit,@RequestParam(value="sort", defaultValue="desc", required=false) String sort) {
@@ -21,25 +30,36 @@ public class UserController {
 	
 	@GetMapping(path="/{userId}", produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}) 
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
-		UserRest returnValue = new UserRest();
-		returnValue.setEmail("test@test.com");
-		returnValue.setFirstName("Tommy");
-		returnValue.setLastName("Versetti");
-		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+		if(users.containsKey(userId)) {
+			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
 	}
 	
-	@PostMapping (consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}, produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+	@PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}, produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
 		UserRest returnValue = new UserRest();
 		returnValue.setEmail(userDetails.getEmail());
 		returnValue.setFirstName(userDetails.getFirstName());
 		returnValue.setLastName(userDetails.getLastName());
+		
+		//simulate DB
+		String userId = UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+		if (users == null) users = new HashMap<>();
+		users.put(userId, returnValue);
+		
 		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
-	@PutMapping
-	public String updateUser() {
-		return "update user was called";
+	@PutMapping(path="/{userId}", consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}, produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails) {
+		UserRest storedUserDetails = users.get(userId);
+		storedUserDetails.setFirstName(userDetails.getFirstName());
+		storedUserDetails.setLastName(userDetails.getLastName());
+		users.put(userId, storedUserDetails);
+		return storedUserDetails;
 	}
 	
 	@DeleteMapping
--- RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/model/request/UpdateUserDetailsRequestModel.java
+++ RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/model/request/UpdateUserDetailsRequestModel.java
@@ -1,50 +1,30 @@
 package pl.lukaszgrymulski.app.ws.ui.model.request;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class UserDetailsRequestModel {
-
-	
+public class UpdateUserDetailsRequestModel {
 	@NotNull(message="First name cannot be null!")
 	@Size(min=3, max=20, message="First name must be 3 to 20 characters long!")
 	private String firstName;
 	
 	@NotNull (message="Last name cannot be null!")
 	private String lastName;
-	
-	@NotNull (message="Email cannot be null!")	
-	@Email ()
-	private String email;
-	
-	@NotNull (message="Password cannot be null!")
-	@Size(min=8,max=16,message="Password must be 8 to 16 characters long")
-	private String password;
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 	
 	
