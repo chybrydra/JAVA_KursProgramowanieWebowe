a4b3b7f62879af2d434c7a10d6ea09520a8fe0de
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-01-25 12:02:03 +0100

    cleaning some mess

---
+++ RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
@@ -0,0 +1,34 @@
+package pl.lukaszgrymulski.app.ws.ui.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users/
+public class UserController {
+	
+	@GetMapping //bind this method to HTTP request
+	public String getUser() {
+		return "get user was called";
+	}
+	
+	@PostMapping //bind this method to HTTP request
+	public String createUser() {
+		return "create user was called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user was called";
+	}
+	
+}
