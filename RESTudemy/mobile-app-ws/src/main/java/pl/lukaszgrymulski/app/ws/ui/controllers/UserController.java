5b3c95ea2cd3d9c1ded6a015b3b50585b0db64e2
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-01-25 12:36:40 +0100

    add default http request values

--- RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
+++ RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
@@ -6,18 +6,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users/
 public class UserController {
 	
-	@GetMapping(path="/{userId}") //bind this method to HTTP request
+	@GetMapping //bind this method to HTTP request
+	public String getUsers(
+			@RequestParam(value="page", defaultValue="1") int page, 
+			@RequestParam(value="limit", defaultValue="50") int limit,
+			@RequestParam(value="sort", defaultValue="desc", required=false) String sort) {
+		return "get users was called with page = " + page 
+				+ ", limit = " + limit 
+				+ ", sort: " + sort;
+	}
+	
+	@GetMapping(path="/{userId}") 
 	public String getUser(@PathVariable String userId) {
 		return "get user was called with userId = " + userId;
 	}
 	
-	@PostMapping //bind this method to HTTP request
+	@PostMapping 
 	public String createUser() {
 		return "create user was called";
 	}
