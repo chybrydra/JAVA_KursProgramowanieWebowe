31bb179acab571be56b5f761adc1325d035b7256
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-01-25 13:32:08 +0100

    make app support Json and XML

--- RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
+++ RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
@@ -1,29 +1,33 @@
 package pl.lukaszgrymulski.app.ws.ui.controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import pl.lukaszgrymulski.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users/
 public class UserController {
 	
 	@GetMapping //bind this method to HTTP request
-	public String getUsers(
+	public UserRest getUsers(
 			@RequestParam(value="page", defaultValue="1") int page, 
 			@RequestParam(value="limit", defaultValue="50") int limit,
-			@RequestParam(value="sort", defaultValue="desc", required=false) String sort) {
-		return "get users was called with page = " + page 
-				+ ", limit = " + limit 
-				+ ", sort: " + sort;
+			@RequestParam(value="sort", defaultValue="desc", required=false) String sort
+			) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@test.com");
+		returnValue.setFirstName("Tommy");
+		returnValue.setLastName("Versetti");
+		returnValue.setUserId("1wD15gG2gE");
+		return returnValue;
 	}
 	
-	@GetMapping(path="/{userId}") 
+	@GetMapping(path="/{userId}", 
+				produces= {
+					MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE
+				}) 
 	public String getUser(@PathVariable String userId) {
 		return "get user was called with userId = " + userId;
 	}
