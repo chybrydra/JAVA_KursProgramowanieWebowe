cbecf4fcdc60cc66ceeb88b2cd92d719af0138a4
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-01-25 15:03:20 +0100

    create POST request body

--- RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
+++ RESTudemy/mobile-app-ws/src/main/java/pl/lukaszgrymulski/app/ws/ui/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import pl.lukaszgrymulski.app.ws.ui.model.request.UserDetailsRequestModel;
 import pl.lukaszgrymulski.app.ws.ui.model.response.UserRest;
 
 @RestController
@@ -12,33 +13,28 @@ import pl.lukaszgrymulski.app.ws.ui.model.response.UserRest;
 public class UserController {
 	
 	@GetMapping //bind this method to HTTP request
-	public String getUsers(
-			@RequestParam(value="page", defaultValue="1") int page, 
-			@RequestParam(value="limit", defaultValue="50") int limit,
-			@RequestParam(value="sort", defaultValue="desc", required=false) String sort
-			) {
+	public String getUsers(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value="limit", defaultValue="50") int limit,@RequestParam(value="sort", defaultValue="desc", required=false) String sort) {
 		return "get users was called with page = " + page 
 				+ ", limit = " + limit
 				+ ", sort = " + sort;
 	}
 	
-	@GetMapping(path="/{userId}", 
-				produces= {
-					MediaType.APPLICATION_XML_VALUE, 
-					MediaType.APPLICATION_JSON_VALUE
-				}) 
+	@GetMapping(path="/{userId}", produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}) 
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		UserRest returnValue = new UserRest();
 		returnValue.setEmail("test@test.com");
 		returnValue.setFirstName("Tommy");
 		returnValue.setLastName("Versetti");
-		returnValue.setUserId("1wD15gG2gE");
-		return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
-	@PostMapping 
-	public String createUser() {
-		return "create user was called";
+	@PostMapping (consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}, produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
 	@PutMapping
